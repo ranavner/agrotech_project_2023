345a74e26096b19e407dcbc220d454385e3f32fa
ranavner <ran.avner5@gmail.com>
2023-06-22 22:00:50 +0300

    copied all codes

---
+++ code/Download_Image_repository.py
@@ -0,0 +1,19 @@
+import gdown
+import time
+import subprocess
+
+def download_image_repository_from_drive():
+    url = "https://drive.google.com/drive/folders/1Ct5QfEFchW4f0ejEFRlJ4ahySA_rOqvj?usp=sharing"
+    gdown.download_folder(url, quiet=True, use_cookies=False)
+
+def analyze_image():
+    subprocess.Popen(["python", "image_color_seperation.py"])
+
+
+def main():
+    download_image_repository_from_drive()
+    analyze_image()
+    time.sleep(600)
+
+if __name__ == "__main__":
+    main()
---
+++ code/csv_Creator.py
@@ -0,0 +1,19 @@
+import csv
+import time
+import os
+from datetime import datetime
+import random
+
+header = ['TIMESTAMP', 'Sensor 1', 'Sensor 2', 'Sensor 3', 'Sensor 4', 'is_motion', 'tmp', 'vpd']
+
+# os.remove('fixed2.csv')
+
+with open('fixed2.csv', 'a') as t:
+    writer = csv.writer(t)
+    writer.writerow(header)
+    for seconds in range(10000):
+        data = [datetime.now(), random.randint(1,100), random.randint(1,100), random.randint(1,100), random.randint(1,100), random.randint(0, 1), random.randint(1,45), random.randint(0, 5)]
+        writer.writerow(data)
+        t.flush()
+
+        time.sleep(1)
