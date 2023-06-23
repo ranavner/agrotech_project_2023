9710e83402fc6f6a6abec0ac5e4dc2f7e57cc9e1
ranavner <ran.avner5@gmail.com>
2023-06-23 14:12:36 +0300

    edited motion sensor file

--- code/Motion_Sensor.ino
+++ code/Motion_Sensor.ino
@@ -1,46 +1,104 @@
-/*********
-  Complete project details at https://randomnerdtutorials.com  
-*********/
+/*
+ * This ESP32 code is created by esp32io.com
+ *
+ * This ESP32 code is released in the public domain
+ *
+ * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-motion-sensor
+ */
 
-// constants won't change. Used here to set a pin number :
-const int ledPin =  26;      // the number of the LED pin
+//-----------------------------------------------------------------------------------
+// wifi settings: 
+#include <WiFi.h>
+#include <Arduino.h>   // Include the Arduino library
+#include <Wire.h>      // Include the Wire library for I2C communication
+#include "ThingSpeak.h"
 
-// Variables will change :
-int ledState = LOW;             // ledState used to set the LED
+// Set the ThingSpeak channel and API key information
+unsigned long myChannelNumber = 2076230;
+const char* myWriteAPIKey = "DDKT7ELV5UW18KYD";
 
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;        // will store last time LED was updated
+// Set the WiFi network credentials
+const char* ssid = "HUJI-guest"; // your wifi SSID name
+const char* password = ""; // wifi password
+//const char* ssid = "Raniphone"; // your wifi SSID name
+//const char* password = "12345670"; // wifi password
 
-// constants won't change :
-const long interval = 1000;           // interval at which to blink (milliseconds)
+
+// Set the ThingSpeak server address
+const char* server = "api.thingspeak.com";
+
+// Create a WiFiClient object to connect to the WiFi network
+WiFiClient client;
+
+// Set the time to wait between uploading data to ThingSpeak (in milliseconds)
+int wait_between_uploads = 5000; // 5 seconds
+
+
+
+//-------------------------------------------------------------------------------------
+
+const int PIN_TO_SENSOR = A4; // A4 pin connected to OUTPUT pin of sensor
+int pinStateCurrent   = LOW;  // current state of pin
+int pinStatePrevious  = LOW;  // previous state of pin
 
 void setup() {
-  // set the digital pin as output:
-  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);            // initialize serial
+    // Disconnect any previous WiFi connection
+  WiFi.disconnect();
+  delay(10);
+
+  // Connect to the WiFi network
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("ESP32 connected to WiFi: ");
+  Serial.println(ssid);
+  Serial.println();
+
+  // Initialize the ThingSpeak library with the WiFi client
+  ThingSpeak.begin(client);
+
+  pinMode(PIN_TO_SENSOR, INPUT); // set ESP32 pin to input mode to read value from OUTPUT pin of sensor
 }
 
 void loop() {
-  // here is where you'd put code that needs to be running all the time.
+  float is_motion = 0;
+  pinStatePrevious = pinStateCurrent; // store old state
+  pinStateCurrent = digitalRead(PIN_TO_SENSOR);   // read new state
 
-  // check to see if it's time to blink the LED; that is, if the
-  // difference between the current time and last time you blinked
-  // the LED is bigger than the interval at which you want to
-  // blink the LED.
-  unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
 
-    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
+  if (pinStatePrevious == LOW && pinStateCurrent == HIGH) {   // pin state change: LOW -> HIGH
+    Serial.println("Motion detected!");
+    is_motion = 1;
+    ThingSpeak.setField(5, is_motion);
+    ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+    delay(10000);
 
-    // set the LED with the ledState of the variable:
-    digitalWrite(ledPin, ledState);
   }
+
+  is_motion = 0;
+  ThingSpeak.setField(5, is_motion);
+  ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  is_motion = 0;
+
+
+  // Print a message to the serial monitor indicating that the data has been uploaded
+  Serial.println("Uploaded to ThingSpeak server.");
+
+  // Disconnect the WiFi client
+  client.stop();
+
+  // Wait for the specified amount of time before uploading the next set of data
+  // thingspeak needs minimum 15 sec delay between updates on a free acount,
+  // a paid acount can update every 1 sec
+  Serial.println("Waiting to upload next reading...");
+  Serial.println();
+  delay(1000);
+
 }
